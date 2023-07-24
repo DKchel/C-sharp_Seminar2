4ab36e39dc037cada4583fc732c112bfb6653868
KDA <kuda74ru@gmail.ru>
2023-07-24 21:58:32 +0500

    inintial commit

---
+++ task01/Program.cs
@@ -0,0 +1,15 @@
+﻿int num = new Random().Next(10, 100);
+
+int digit1 = num / 10;
+int digit2 = num % 10;
+
+int max = digit1;
+
+if (digit2 > max)
+{
+    max = digit2;
+}
+
+Console.WriteLine(num);
+Console.WriteLine();
+Console.WriteLine(max);
